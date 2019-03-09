94e8f5dff897d42ecf75622b85166e6ec6f894a3
Horváth Martin <sonyc444@gmail.com>
2019-03-09 12:07:25 +0100

    user/listgroups endpoint

--- Backend/NodeJS Server/routes/UsersController.ts
+++ Backend/NodeJS Server/routes/UsersController.ts
@@ -1,5 +1,7 @@
 import {BaseController} from "../core/BaseController";
 import {LoginResponse, LoginState} from "../core/LoginResponse";
+import {ObjectId} from "bson";
+import {StatusCodes} from "../core/StatusCodes";
 const hasher = require('../core/Hasher');
 
 export class UsersController extends BaseController {
@@ -18,17 +20,17 @@ export class UsersController extends BaseController {
                         ] },function(err, user) {
                     // In case the user not found
                     if(err) {
-                        res.status(500).send(err);
+                        res.status(StatusCodes.InternalError).send(err);
                         return;
                     }
                     if (user){
                         // TODO: respond with JWT token
-                        res.status(200).send(new LoginResponse(
+                        res.status(StatusCodes.OK).send(new LoginResponse(
                             LoginState.OK,
                             'jaudiem1l3928421najxkfpdnwnejrud82k3xa94')
                         );
                     } else {
-                        res.status(401).send(new LoginResponse(
+                        res.status(StatusCodes.Unauthorized).send(new LoginResponse(
                             LoginState.BadAuth,
                             '')
                         );
@@ -36,12 +38,49 @@ export class UsersController extends BaseController {
                 });
             });
 
-        // TODO: remove from production, only use this for testing!
-        this.router.get('/list', async (req, res) => {
-            let docs = await this.getDb().collection("users").find().toArray();
+        this.router.get('/listgroups', async (req, res) => {
+                // TODO: get token from session
+                const userId = this.authenticator.check('TODOTOKEN');
+
+                if(userId == null) {
+                    res.status(StatusCodes.Forbidden).send("");
+                    return;
+                }
+
+                this.getDb().collection("users").findOne(
+                    {_id:userId},
+                    {fields:{_id:false,groups:true}},
+                    function (error, result) {
+                        if(error) {
+                            res.status(StatusCodes.InternalError).send(error);
+                            return;
+                        }
+
+                        if(result){
+                            res.status(StatusCodes.OK).send(result);
+                        }
+                        else{
+                            res.status(StatusCodes.InternalError).send(result);
+                        }
+                    }
+                );
+        });
+
+        this.router.put('/addgroup', async (req, res) => {
+            const userId: ObjectId = new ObjectId(req.query.userid);
+            const groupId: ObjectId = new ObjectId(req.query.groupid);
+
+            console.log(userId);
+            console.log(groupId);
+
+            let docs = await this.getDb().collection("users").updateOne(
+                {_id:userId},
+                {$addToSet:{groups:groupId}}
+
+            );
 
             console.log(docs);
-            res.send(docs);
         });
+
     }
 }
