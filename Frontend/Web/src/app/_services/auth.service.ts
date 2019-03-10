f818c48595145ec3d36c191a4fe1fac38c7d5883
Peter Horvath <phorvath90@gmail.com>
2019-03-10 15:05:19 +0100

    Angular: Implement authentication service with JWT

--- Frontend/Web/src/app/_services/auth.service.ts
+++ Frontend/Web/src/app/_services/auth.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { User } from '../_models/user';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  currentUserSubject: BehaviorSubject<User>;
+  currentUser: Observable<User>;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  login(username: string, password: string) {
+    return this.http.post<any>(`${environment.API_URL}/login`, { username, password })
+      .pipe(map(user => {
+        if (user && user.token) {
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+        }
+
+        return user;
+      }));
+  }
+
+  logout() {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
 }
