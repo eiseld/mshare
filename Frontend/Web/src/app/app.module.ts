f1aef681c82c4742e687b190530020bbe2ba22e4
Peter Horvath <phorvath90@gmail.com>
2019-03-10 19:31:51 +0100

    Angular: Add login form
    Login form with input validation added.
    Login API URL should be updated based on Backend.

--- Frontend/Web/src/app/app.module.ts
+++ Frontend/Web/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,6 +32,7 @@ import { GroupDetailComponent } from './components/group-detail/group-detail.com
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
