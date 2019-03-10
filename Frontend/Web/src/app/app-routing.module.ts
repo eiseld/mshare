b07a8bc3fd3633c1d667f9980b00cc8472d0a89e
Peter Horvath <phorvath90@gmail.com>
2019-03-10 15:41:05 +0100

    Angular: Add auth guard to home page

--- Frontend/Web/src/app/app-routing.module.ts
+++ Frontend/Web/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { AboutComponent } from './components/about/about.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent },
-  { path: 'about', component: AboutComponent }
+  { path: '', component: HomepageComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
