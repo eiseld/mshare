a6851110d77571c6c548afe187e9cc5f999f7288
Peter Horvath <phorvath90@gmail.com>
2019-03-10 12:43:03 +0100

    Angular: Add HTTP API service

--- Frontend/Web/src/app/app.module.ts
+++ Frontend/Web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,6 +25,7 @@ import { GroupDetailComponent } from './group-detail/group-detail.component';
     GroupDetailComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
