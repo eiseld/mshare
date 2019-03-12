8891a8dbdcab6d2b5e2bdd995e2167d017464348
atus89123 <toth.ata11@gmail.com>
2019-03-12 14:44:28 +0100

    Architecture components defined, login screen done.

--- Frontend/Android/app/src/main/java/elte/moneyshare/model/APIClient.kt
+++ Frontend/Android/app/src/main/java/elte/moneyshare/model/APIClient.kt
@@ -1,4 +1,37 @@
 package elte.moneyshare.model
 
+import android.content.Context
+import elte.moneyshare.R
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object APIClient {
+
+    private lateinit var baseUrlValue: String
+    private lateinit var retrofit: Retrofit
+    private lateinit var apiDefinition: APIDefinition
+    private lateinit var repository: Repository
+
+    fun init(context: Context) {
+        baseUrlValue = context.getString(R.string.base_url_value)
+
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
+        retrofit = Retrofit.Builder()
+            .baseUrl(baseUrlValue)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        apiDefinition = retrofit.create(APIDefinition::class.java)
+        repository = Repository(apiDefinition)
+    }
+
+    fun getRepository(): Repository{
+        return repository
+    }
 }
--- Frontend/Android/app/src/main/java/elte/moneyshare/viewmodel/LoginViewModel.kt
+++ Frontend/Android/app/src/main/java/elte/moneyshare/viewmodel/LoginViewModel.kt
@@ -1,4 +1,17 @@
 package elte.moneyshare.viewmodel
 
-class LoginViewModel {
+import android.arch.lifecycle.ViewModel
+import elte.moneyshare.model.APIClient
+
+class LoginViewModel: ViewModel() {
+
+    fun postLoginUser(email: String, password: String, completion: (response: String?, error: String?) -> Unit) {
+        APIClient.getRepository().postLoginUser(email, password) { response, error ->
+            if(error == null) {
+                completion(response, null)
+            } else {
+                completion(null, error)
+            }
+        }
+    }
 }
