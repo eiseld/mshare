7e7262c6aacbcf4af16c1bb06dfb2f4c7850f741
ZombieDroid <sza.gabor66@gmail.com>
2019-03-15 15:17:06 +0100

    MSHARE-14

---
+++ Frontend/Web/src/app/components/forgottenpw/forgottenpw.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forgottenpw',
+  templateUrl: './forgottenpw.component.html',
+  styleUrls: ['./forgottenpw.component.css']
+})
+export class ForgottenpwComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
