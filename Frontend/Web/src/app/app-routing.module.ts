a4da1899f556b5bb1207515acbb6f9b0aa4dceed
ZombieDroid <sza.gabor66@gmail.com>
2019-03-18 18:48:30 +0100

    MSHARE-14 Elfelejtett jelszó

--- Frontend/Web/src/app/app-routing.module.ts
+++ Frontend/Web/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomepageComponent } from './components/homepage/homepage.component';
 import { AboutComponent } from './components/about/about.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './guards/auth.guard';
+import {ForgottenpwComponent} from "./components/forgottenpw/forgottenpw.component";
 
 const routes: Routes = [
   { path: '', component: HomepageComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'fpwd', component: ForgottenpwComponent},
   { path: '**', redirectTo: '' }
 ];
 
--- Frontend/Web/src/app/components/forgottenpw/forgottenpw.component.ts
+++ Frontend/Web/src/app/components/forgottenpw/forgottenpw.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {debounceTime, first} from "rxjs/operators";
+import {ForgottenpwService} from "../../services/forgottenpw/forgottenpw.service";
+import {Subject} from "rxjs/internal/Subject";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-forgottenpw',
@@ -7,9 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgottenpwComponent implements OnInit {
 
-  constructor() { }
+  //constructor(private forgottenService: ForgottenpwService) { }
+  email: string;
+  private _success = new Subject<string>();
+  staticAlertClosed = false;
+  successMessage: string;
+  type: string;
 
-  ngOnInit() {
+  ngOnInit(): void {
+    setTimeout(() => this.staticAlertClosed = true, 20000);
+
+    this._success.subscribe((message) => this.successMessage = message);
+    this._success.pipe(
+      debounceTime(5000)
+    ).subscribe(() => this.successMessage = null);
+  }
+
+
+  psResetForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.psResetForm = fb.group({
+      'email': [null, Validators.compose([Validators.required, Validators.email])]
+    });
+  }
+
+  makeRequestToResetLink(formData, valid: boolean) {
+    if (valid) {
+      this._success.next(`Amennyiben a megadott e-mail cím regisztrálva van nálunk, arra hamarosan megérkezik a jelszó-emlékeztető..`);
+      this.type='success';
+    }else {
+      this._success.next(`A megadtt email formátuma nem megfelelő!`);
+      this.type='danger';
+    }
+    // this.submitted = true;
+    // if (this.loginForm.invalid) {
+    //   return;
+    // }
+    //
+    // this.forgottenService.sendForgottenPwMail(t)
+    // this.authService.login(this.loginForm.controls.username.value, this.loginForm.controls.password.value)
+    //   .pipe(first())
+    //   .subscribe(
+    //     data => {
+    //       this.router.navigate([this.returnUrl]);
+    //     },
+    //     error => {
+    //       this.error = "Helytelen felhasználónév vagy jelszó";
+    //       this.loading = false;
+    //     });
   }
 
 }
