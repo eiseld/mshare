6962cf2b7304b29f92d1ff89d6f0ec9c08437aef
Somogyi Ákos <s.akos@live.com>
2019-04-28 02:41:53 +0200

    Angular: spending-creator component bugfixes: textstyles, list update, debt roundings, debt auto calculation.

--- frontend/web/src/app/components/group-detail/group-detail.component.ts
+++ frontend/web/src/app/components/group-detail/group-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, ChangeDetectionStrategy } from '@angular/core';
 import { GroupInfo, GroupData, MemberData } from '../group-manager/group-manager.component';
 import { Spending } from '../spending-creator/spending-creator.component'
 import { Output, EventEmitter } from '@angular/core'; 
@@ -6,15 +6,20 @@ import { Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-group-detail',
   templateUrl: './group-detail.component.html',
-  styleUrls: ['./group-detail.component.css']
+  styleUrls: ['./group-detail.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class GroupDetailComponent implements OnInit {
+export class GroupDetailComponent implements OnChanges {
   @Input() groupData: GroupData;
   @Input() spendings: Spending[];
   calculatedSpendings: CalculatedSpending[]=[];
   pages={groupMemberDetails:0,groupSpendingDetails:1};
   selectedPage=this.pages.groupMemberDetails;
   @Output() startSpendingCreation = new EventEmitter();
+
+  ngOnChanges(){
+    this.showPage(this.selectedPage);
+  }
 
   startCreateSpending() {
     this.startSpendingCreation.next(new GroupInfo(this.groupData));
