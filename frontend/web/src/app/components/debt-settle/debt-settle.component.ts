11a06b5b08026929dc7b849243d7fda09832f074
Somogyi Ákos <s.akos@live.com>
2019-04-30 02:58:56 +0200

    Angular: debt-settle component connected to backend

--- frontend/web/src/app/components/debt-settle/debt-settle.component.ts
+++ frontend/web/src/app/components/debt-settle/debt-settle.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { MemberData } from '../group-manager/group-manager.component';
+import { MemberData, GroupData } from '../group-manager/group-manager.component';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-debt-settle',
@@ -7,15 +10,16 @@ import { MemberData } from '../group-manager/group-manager.component';
   styleUrls: ['./debt-settle.component.css']
 })
 export class DebtSettleComponent implements OnInit {
+  @Input() groupData:GroupData;
   @Input() selectedMember:MemberData;
   @Output() stopSettleDebtEvent = new EventEmitter();
   @Output() updateSelectedGroupEvent = new EventEmitter();
-
+  error:string="";
   updateSelectedGroup() {
     this.updateSelectedGroupEvent.next();
   }
 
-  constructor() { }
+  constructor(private http: HttpClient, private authenticationService: AuthService) { }
 
   ngOnInit() {
   }
@@ -30,10 +34,34 @@ export class DebtSettleComponent implements OnInit {
   }
 
   settleDebt(){
-    if(this.selectedMember.balance!=0){
-      //TODO connect to backend to settle debt, get update GroupData
-      this.updateSelectedGroup();
-    }
-    this.unselectMember();
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    this.http.get<any>(`${environment.API_URL}/profile/`,
+      httpOptions).subscribe(
+        data => {
+          if(this.selectedMember.balance<0){
+            this.http.get<any>(`${environment.API_URL}/group/debtsettlement?debtorid=`+data.id+`&lenderid=`+this.selectedMember.id+`&groupid=`+this.groupData.id,
+            httpOptions).subscribe(
+              data => {this.updateSelectedGroup(); this.unselectMember();},
+              error => {this.error="Sikertelen a tartozás rendezése!"}
+            );
+          }
+          else if(this.selectedMember.balance>0){
+            this.http.get<any>(`${environment.API_URL}/group/debtsettlement?debtorid=`+this.selectedMember.id+`&lenderid=`+data.id+`&groupid=`+this.groupData.id,
+            httpOptions).subscribe(
+              data => {this.updateSelectedGroup(); this.unselectMember();},
+              error => {this.error="Sikertelen a tartozás rendezése!"}
+            );
+          }
+          else{
+            this.updateSelectedGroup();
+            this.unselectMember();
+          }
+        },
+        error => {this.error="Sikertelen a tartozás rendezése!"}
+      );
   }
 }
