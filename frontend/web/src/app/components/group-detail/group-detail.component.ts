b99ca814a0fc596d00992f0558c642b15e29ca44
Soma <soma.szeier@gmail.com>
2019-05-06 22:33:16 +0200

    Added member removal, sorted member list

--- frontend/web/src/app/components/group-detail/group-detail.component.ts
+++ frontend/web/src/app/components/group-detail/group-detail.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input, OnChanges, ChangeDetectionStrategy } from '@angular/core';
-import { GroupInfo, GroupData, MemberData } from '../group-manager/group-manager.component';
+import { GroupInfo, GroupData, MemberData, GroupManagerComponent } from '../group-manager/group-manager.component';
 import { Spending } from '../spending-creator/spending-creator.component'
 import { Output, EventEmitter } from '@angular/core'; 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment'
 
 @Component({
   selector: 'app-group-detail',
@@ -12,20 +14,60 @@ import { Output, EventEmitter } from '@angular/core';
 export class GroupDetailComponent implements OnChanges {
   @Input() groupData: GroupData;
   @Input() spendings: Spending[];
+  currentUser : MemberData;
+  sortedMembers: MemberData[] = [];
   calculatedSpendings: CalculatedSpending[]=[];
-  pages={groupMemberDetails:0,groupSpendingDetails:1};
+  pages={groupMemberDetails:0,groupSpendingDetails:1,groupManageMembers:2};
   selectedPage=this.pages.groupMemberDetails;
   @Output() startSpendingCreation = new EventEmitter();
+  @Output() updateSelectedGroupEvent = new EventEmitter();
 
   ngOnChanges(){
-    this.showPage(this.selectedPage);
+    if(this.groupData != null)
+    {
+    this.sortedMembers = this.groupData.members.sort(
+      (a,b) => {
+        if(a.name > b.name)
+          return 1;
+        if(a.name < b.name)
+          return -1;
+        return 0;
+      }
+      )
+    }
+    if(this.selectedPage == this.pages.groupManageMembers && this.currentUser.name != this.groupData.creator.name)
+    {
+      this.showPage(this.pages.groupMemberDetails);
+    }
+    else
+      this.showPage(this.selectedPage);
+  }
+
+  
+  getCurrentUser(){
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    this.http.get<any>(`${environment.API_URL}/profile/`, httpOptions).subscribe(
+      data => { this.currentUser=data; },
+      error => {}
+    );
   }
 
   startCreateSpending() {
     this.startSpendingCreation.next(new GroupInfo(this.groupData));
   }
 
-  constructor() { }
+  addNewMember() {
+
+  }
+
+
+  constructor(private http: HttpClient) {
+
+  }
 
   calcDefaultDebts(){
     if(this.calculatedSpendings!=undefined){
@@ -59,8 +101,21 @@ export class GroupDetailComponent implements OnChanges {
     }
   }
 
-  ngOnInit() {
+  deleteFromGroup(id: number, groupid: number)
+  {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+	this.http.delete(`${environment.API_URL}/group/${groupid}/members/remove/${id}`, httpOptions)
+	.subscribe(data => {this.updateSelectedGroupEvent.next()}, error => { });
+    this.selectedPage = this.pages.groupMemberDetails;
+  }
 
+  ngOnInit() {
+    this.getCurrentUser();
+    this.selectedPage = this.pages.groupMemberDetails; 
   }
 
   showPage(selectedPage){
@@ -68,6 +123,7 @@ export class GroupDetailComponent implements OnChanges {
     if(this.selectedPage==this.pages.groupSpendingDetails){
       this.calcDefaultDebts();
     }
+   
   }
 }
 
