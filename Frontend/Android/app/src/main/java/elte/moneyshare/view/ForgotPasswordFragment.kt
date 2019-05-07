9742651219c9230c50cec17855989ee59f8f5d2f
ZombieDroid <sza.gabor66@gmail.com>
2019-05-07 10:32:32 +0200

    Javitás

--- Frontend/Android/app/src/main/java/elte/moneyshare/view/ForgotPasswordFragment.kt
+++ Frontend/Android/app/src/main/java/elte/moneyshare/view/ForgotPasswordFragment.kt
@@ -33,10 +33,10 @@ class ForgotPasswordFragment : Fragment() {
             //viewModel.postLoginUser("test1@test.hu", "default") { response, error ->
             viewModel.putForgotPassword(emailEditText.text.toString()) { response, error ->
                 if(error == null) {
-                    Toast.makeText(context, response, Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context, context?.getString(R.string.email_sent), Toast.LENGTH_SHORT).show()
                     activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.frame_container, GroupsFragment())?.commit()
                 } else {
-                    Toast.makeText(context, error, Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context, context?.getString(R.string.email_sent), Toast.LENGTH_SHORT).show()
                 }
             }
         }
