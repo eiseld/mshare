aaf1755b3420464160e535e6fe82291525f378f3
atus89123 <toth.ata11@gmail.com>
2019-05-10 23:42:49 +0200

    backend deleted.

--- backend/database/export.bat
+++
@@ -1,2 +0,0 @@
-docker exec -i mshare-mysql mysqldump --events --routines --triggers --user=root --password=ilovescrum mshare > data.sql
-PAUSE
--- backend/database/import.bat
+++
@@ -1,3 +0,0 @@
-docker exec -it mshare-mysql bash -c "mysql --user=root --password=ilovescrum --execute=\"drop DATABASE if exists mshare;CREATE DATABASE mshare;\""
-docker exec -i mshare-mysql mysql --user=root --password=ilovescrum mshare < data.sql
-PAUSE
