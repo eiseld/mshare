451d6df51c6abbd7b3754e830d0af0c5ade9f186
Somogyi Ákos <s.akos@live.com>
2019-05-11 19:40:56 +0200

    Backend: fixed logo in e-mail

--- backend/aspnet/MShare_ASP/Services/EmailService.cs
+++ backend/aspnet/MShare_ASP/Services/EmailService.cs
@@ -11,9 +11,11 @@ namespace MShare_ASP.Services {
 
         private MailboxAddress _senderAddr;
         private Configurations.IEmailConfiguration _emailConf;
+        private Configurations.IURIConfiguration _uriConf;
 
-        public EmailService(Configurations.IEmailConfiguration emailConf) {
+        public EmailService(Configurations.IEmailConfiguration emailConf, Configurations.IURIConfiguration uriConf) {
             _emailConf = emailConf;
+            _uriConf = uriConf;
             _senderAddr = new MailboxAddress(_emailConf.Name, _emailConf.Address);
         }
 
@@ -23,27 +25,11 @@ namespace MShare_ASP.Services {
             msg.To.Add(new MailboxAddress(name, targetEmail));
             msg.Subject = subject;
 
-            var filePath = "./Resources/MoneyShareLogo_128px.png";
-            var logo = new MimePart("image", "png")
-            {
-                Content = new MimeContent(System.IO.File.OpenRead(filePath), ContentEncoding.Default),
-                ContentDisposition = new ContentDisposition(ContentDisposition.Inline),
-                ContentTransferEncoding = ContentEncoding.Base64,
-                FileName = System.IO.Path.GetFileName(filePath),
-                ContentId="logoImage"
-            };
-
             msg.Body = new TextPart(TextFormat.Html)
             {
-                Text = $"<html><h1><img src='{logo.ContentId}' alt='MoneyShare logo'></img> MoneyShare</h1><p>{message}</p> </html>"
+                Text = $"<html><h1><img src='{_uriConf.URIForEndUsers}/assets/MoneyShareLogo_128px.png' alt='MoneyShare logo'></img> MoneyShare</h1><p>{message}</p> </html>"
             };
-            
-            var multipart = new Multipart("mixed");
-            multipart.Add(logo);
-            multipart.Add(msg.Body);
-
-            msg.Body = multipart;
-
+ 
             using (var client = new SmtpClient()) {
                 client.ServerCertificateValidationCallback = (s, c, ch, e) => true;
 
