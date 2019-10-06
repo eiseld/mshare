2ab71e7059331e431954388b62aa3c7f1a5d6bf1
Ata <toth.ata11@gmail.com>
2019-10-06 22:36:30 +0200

    Add lang updates to repo and models.

--- Frontend/Android/app/src/main/java/elte/moneyshare/entity/ForgottenPasswordData.kt
+++ Frontend/Android/app/src/main/java/elte/moneyshare/entity/ForgottenPasswordData.kt
@@ -1,9 +1,11 @@
 package elte.moneyshare.entity
 
 import android.os.Parcelable
+import elte.moneyshare.SharedPreferences
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class ForgottenPasswordData (
-    var email: String
+    var email: String,
+    val lang : String = SharedPreferences.lang
 ) : Parcelable
