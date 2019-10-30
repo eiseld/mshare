f923e3c0bc82770be1c412388d15402d0a24e482
bekezoltan00 <beke.zoltan00@gmail.com>
2019-10-30 22:42:08 +0100

    Auto login removed

--- frontend/android/app/src/main/java/elte/moneyshare/view/LoginFragment.kt
+++ frontend/android/app/src/main/java/elte/moneyshare/view/LoginFragment.kt
@@ -45,8 +45,8 @@ class LoginFragment : Fragment() {
         loginButton.setOnClickListener {
             val email = emailEditText.text.toString()
             val password = passwordEditText.text.toString()
-            viewModel.putLoginUser("test1@test.hu", "default") { response, error ->
-            //viewModel.putLoginUser(email, password) { response, error ->
+            //viewModel.putLoginUser("test1@test.hu", "default") { response, error ->
+            viewModel.putLoginUser(email, password) { response, error ->
                 if (error == null) {
                     if(stayLoggedInCheckBox.isChecked)
                     {
