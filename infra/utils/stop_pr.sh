2019e3eaef2faf0c0d547ae8dacd3cc2c92bd955
hrvthp <hrvth0@gmail.com>
2019-11-03 19:51:59 +0100

    Infra: Update pull request build scripts

--- infra/utils/stop_pr.sh
+++ infra/utils/stop_pr.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-db="${1:-jenkins.db}"
-n="${2:-0}"
-(export pr_port=809${n} pr_version=${n}; cd ../docker/; sudo -E docker-compose -f docker-compose-pr.yml down; sudo -E docker-compose -f docker-compose-pr.yml remove)
-sqlite3 ${db) "update pr set instance_count = 0 where id = ${n}"
+git fetch origin +refs/pull/${pr_name}/merge
+git checkout FETCH_HEAD
+n=$(sqlite3 ${db} "select id from pr where pr = '${pr_name}' LIMIT 1")
+(export pr_port=809${n} pr_version=${n}; cd ./infra/docker/; sudo -E docker-compose -f docker-compose-pr.yml down)
+sqlite3 ${db} "update pr set instance_count = 0, pr = '' where id = ${n}"
