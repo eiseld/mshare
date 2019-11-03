02db5956bb007988b29c1a5667b0ac1f6768bbea
bekezoltan00 <beke.zoltan00@gmail.com>
2019-11-03 13:28:32 +0100

    MSHARE-124 BugFix - 'Bejelentkezésnél rossz formátumú felhasználónév vagy jelszó hibaüzenet'

--- frontend/android/app/src/main/java/elte/moneyshare/view/LoginFragment.kt
+++ frontend/android/app/src/main/java/elte/moneyshare/view/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -24,6 +25,18 @@ class LoginFragment : Fragment() {
 
     private lateinit var viewModel: LoginViewModel
 
+    fun passwordValidator(txt: String): String {
+        var pwdError = ""
+        context?.let {
+            if(txt.length <6)
+            {
+                pwdError = it.getString(R.string.minimum_characters).plus('\n')
+            }
+        }
+
+        return pwdError
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -41,6 +54,48 @@ class LoginFragment : Fragment() {
             activity?.finish()
         }
         viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
+
+        emailEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                val email = emailEditText.text.toString()
+                val password = passwordEditText.text.toString()
+                var emailValid = Patterns.EMAIL_ADDRESS.matcher(email).matches()
+                if(!emailValid)
+                {
+                    emailEditText.error = context?.getString(R.string.email_not_correct)
+                    loginButton.isEnabled = false
+                } else if (passwordValidator(password).isEmpty()){
+                    emailEditText.error = null
+                    loginButton.isEnabled = true
+                }
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
+
+        passwordEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                val txt = passwordEditText.text.toString()
+                val email = emailEditText.text.toString()
+                val pwdError = passwordValidator(txt)
+                if (pwdError.length > 1) {
+                    passwordEditText.error = pwdError
+                    loginButton.isEnabled = false
+                } else if (Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    passwordEditText.error = null
+                    loginButton.isEnabled = true
+                }
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
 
         loginButton.setOnClickListener {
             val email = emailEditText.text.toString()
