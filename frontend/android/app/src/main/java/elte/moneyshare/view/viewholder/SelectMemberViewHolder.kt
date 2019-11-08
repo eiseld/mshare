9e09c667cd3a7b526000f5b67be67b2db78380c3
aa <aaa@a.hu>
2019-11-08 22:42:53 +0100

    Bugfix for missing currencies

--- frontend/android/app/src/main/java/elte/moneyshare/view/viewholder/SelectMemberViewHolder.kt
+++ frontend/android/app/src/main/java/elte/moneyshare/view/viewholder/SelectMemberViewHolder.kt
@@ -12,5 +12,6 @@ class SelectMemberViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     var memberRootLayout: ConstraintLayout = itemView.findViewById(R.id.memberRootLayout)
     var memberNameTextView: TextView = itemView.findViewById(R.id.memberNameTextView)
     var memberSpendingEditText: TextView = itemView.findViewById(R.id.memberSpendingEditText)
+    var currencyTextView : TextView = itemView.findViewById(R.id.currency_TextView)
     var selectedMemberCheckBox: CheckBox = itemView.findViewById(R.id.selectedMemberCheckBox)
 }
