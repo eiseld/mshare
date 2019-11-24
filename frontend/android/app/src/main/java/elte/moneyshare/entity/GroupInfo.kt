9147efedbb830c902b4d0daf9de27a5713f22845
aa <aaa@a.hu>
2019-11-24 22:28:49 +0100

    Group ordering added

--- frontend/android/app/src/main/java/elte/moneyshare/entity/GroupInfo.kt
+++ frontend/android/app/src/main/java/elte/moneyshare/entity/GroupInfo.kt
@@ -1,9 +1,12 @@
 package elte.moneyshare.entity
 
+import java.util.*
+
 data class GroupInfo (
     var id: Int,
     var name: String,
     var creator: String,
     var memberCount: Int,
-    var myCurrentBalance: Int
+    var myCurrentBalance: Int,
+    var lastModified : String
 )
