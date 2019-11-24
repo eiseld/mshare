8330451dd2cf36fa37ebfb51f1c63eb9ffd1fa5f
aa <aaa@a.hu>
2019-11-24 12:06:00 +0100

    Textinputs modified for forgotten password and new passwords screens.
    The error will be shown only after clicking on the button.

--- frontend/android/app/src/main/java/elte/moneyshare/view/NewPasswordFragment.kt
+++ frontend/android/app/src/main/java/elte/moneyshare/view/NewPasswordFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,12 +18,42 @@ import elte.moneyshare.util.Action
 import elte.moneyshare.util.convertErrorCodeToString
 import elte.moneyshare.util.showToast
 import elte.moneyshare.viewmodel.NewPasswordViewModel
+import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.fragment_new_password.*
+import kotlinx.android.synthetic.main.fragment_new_password.passwordEditText
+import kotlinx.android.synthetic.main.fragment_new_password.passwordTextInputLayout
 
 class NewPasswordFragment : Fragment() {
 
     private lateinit var viewModel: NewPasswordViewModel
     private var token: String? = null
+
+    fun passwordValidator(txt: String): String {
+        var pwdError = ""
+        val uppercaseRegex = """[A-Z]""".toRegex()
+        val lowercaseRegex = """[a-z]""".toRegex()
+        val numberRegex    = """[0-9]""".toRegex()
+        context?.let {
+            if(txt.length <6)
+            {
+                pwdError = it.getString(R.string.minimum_characters).plus('\n')
+            }
+            if(!txt.contains(uppercaseRegex))
+            {
+                pwdError = pwdError.plus(it.getString(R.string.uppercase_required).plus('\n'))
+            }
+            if(!txt.contains(lowercaseRegex))
+            {
+                pwdError = pwdError.plus(it.getString(R.string.lowercase_required).plus('\n'))
+            }
+            if(!txt.contains(numberRegex))
+            {
+                pwdError = pwdError.plus(it.getString(R.string.number_required).plus('\n'))
+            }
+        }
+
+        return pwdError
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,34 +68,30 @@ class NewPasswordFragment : Fragment() {
 
         viewModel = ViewModelProviders.of(this).get(NewPasswordViewModel::class.java)
 
-        passwordEditText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(pw: Editable?) {
-                if (pw.toString().length < 6) {
-                    passwordEditText.error = getString(R.string.minimum_characters)
-                    confirmPasswordButton.disable()
-                } else {
-                    passwordEditText.error = null
-                    confirmPasswordButton.enable()
-                }
-            }
-
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-        })
-
         confirmPasswordButton.setOnClickListener {
-            token?.let {
-                viewModel.putNewPassword(passwordEditText.text.toString(), it) { _, error ->
-                    if (error == null) {
-                        context?.let { getString(R.string.new_password_updated).showToast(it) }
-                    } else {
-                        DialogManager.showInfoDialog(error.convertErrorCodeToString(Action.AUTH_LOGIN, context), context)
+            val password = passwordEditText.text.toString()
+            var err = false
+            val pwdError = passwordValidator(password)
+            if (pwdError.length > 1) {
+                passwordTextInputLayout.error = pwdError
+                err = true
+            }
+            else {
+                passwordTextInputLayout.error = null
+            }
+            if(!err)
+            {
+                token?.let {
+                    viewModel.putNewPassword(passwordEditText.text.toString(), it) { _, error ->
+                        if (error == null) {
+                            context?.let { getString(R.string.new_password_updated).showToast(it) }
+                        } else {
+                            DialogManager.showInfoDialog(error.convertErrorCodeToString(Action.AUTH_LOGIN, context), context)
+                        }
                     }
                 }
             }
+
         }
     }
 }
